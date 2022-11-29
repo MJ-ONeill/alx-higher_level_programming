85ce1799cd14789f7cce311b7cfc5023ef41843e
MJ O Neill <mjoneill20100@gmail.com>
2022-11-29 11:35:41 -0800

    last fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,4 +4,4 @@ for letter in range(25, -1, -1):
     alphabet = letter + ord('A')
     if letter % 2 == 1:
         alphabet += 32
-    print('{:c}'.format(alphabet, end='')
+    print('{:c}'.format(alphabet), end='')
