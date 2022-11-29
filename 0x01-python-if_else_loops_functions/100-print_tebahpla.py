53aeea8dc435504eda8c0e1be86aceea917a9cf3
MJ O Neill <mjoneill20100@gmail.com>
2022-11-29 11:06:43 -0800

    initial-commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-for c in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(c - 33)), end="")
+for letter in range(25, -1, -1):
+    alphabet = letter + ord('A')
+    if letter % 2 == 1:
+        alphaber += 32
+    print(''{:c}'.format(alphabet, end='')
