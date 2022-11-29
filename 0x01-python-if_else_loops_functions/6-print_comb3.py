18cdeca4cd3359e833fff532b7be740e8055224a
MJ O Neill <mjoneill20100@gmail.com>
2022-11-29 11:29:39 -0800

    last fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for ones in range(0, 10):
     for tens in range(0, 10):
         if ones < tens:
             if ones < 8:
-                print('{}'.format(ones, tens), end=', ')
+                print('{}{}'.format(ones, tens), end=', ')
             else:
-            print('{}{}'.format(ones, tens))
+                print('{}{}'.format(ones, tens))
