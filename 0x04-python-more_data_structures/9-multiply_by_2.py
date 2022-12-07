ea69b105dd8e90189b5d8e078e5e95c4d791117b
MJ O Neill <mjoneill20100@gmail.com>
2022-12-07 05:46:14 -0800

    commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
-#!/usr/biin/python3
+#!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    """Return a new dictionary wirh all values multiplied ny 2."""
-    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
+    new_d = {}
+    for i in a_dictionary:
+        new_d[i] = a_dictionary[i] * 2
+    return new_d
