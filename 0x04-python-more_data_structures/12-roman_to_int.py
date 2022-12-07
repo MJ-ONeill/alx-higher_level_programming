1b2bb541fcf2b77911e364e72d7556e3725b0560
MJ O Neill <mjoneill20100@gmail.com>
2022-12-07 09:10:39 -0800

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if type(roman_string) != str or roman_string is None:
         return 0
     roman_d = {'X': 10, 'VII': 7, 'IX': 9, 'LXXXVII': 87, 'DCCVII': 707}
-    roman_n = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
-            roman_n += roman_d[roman_string[j]] - 2 * \
-                        roman_d[roman_string[j - 1]
+    list = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman_d[roman_string[i]] > roman_d[roman_string[i - 1]]:
+            list == roman_d[roman_string[i]] - 2 * roman_d[roman_string[i - 1]]
         else:
-            roman_n += roman_d[roman_string[j]]
-        return roman_n
+            list += roman_d[roman_string[i]]
+    return list
