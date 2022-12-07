42d4a0c3c0f537598c7e74c07fe0170d4c3e7571
MJ O Neill <mjoneill20100@gmail.com>
2022-12-07 07:28:30 -0800

    100 commit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
+    """Return the weigheted average of all integers in a list of tuples."""
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
     average = 0
-    div = 0
+    size = 0
     for tup in my_list:
-        average += tup[1]
-    return float(average / div)
+        average += (tup[0] * tup[1])
+        size += tup[1]
+    return (average / size)
