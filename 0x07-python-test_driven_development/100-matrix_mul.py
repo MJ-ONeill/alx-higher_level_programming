816e31e0947d59bf03b25a4cfe28dd5610695d18
MJ O'Neill <91608528+MJ-ONeill@users.noreply.github.com>
2022-12-30 19:24:51 +0200

    commit fix

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -17,7 +17,7 @@ def matrix_mul(m_a, m_b):
     Raises:
         TypeError: If m_a or m_b is not a list
         TypeError: If m_a or m_b is not a list of lists
-        TypeError: If one elements of list of lists is not int/float
+        TypeError: If one element of list of lists is not int/float
         TypeError: If row of m_a or m_b are not the same size
         ValueError: If m_a or m_b is empty
         ValueError: If m_a and m_b cannot be multiplied
@@ -30,7 +30,7 @@ def matrix_mul(m_a, m_b):
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
-        raise TypeError("m_b must be a lsit")
+        raise TypeError("m_b must be a list")
 
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
@@ -43,10 +43,10 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_b can't be empty")
 
     if not all((isinstance(element, int) or isinstance(element, float))
-                for element in [number for row in m_a for number in row]):
+               for element in [number for row in m_a for number in row]):
         raise TypeError("m_a should contain only integers or floats")
-    if not all((isinstance(element,  int) or isinstance(element, float))
-                for element in [number for row in m_b for number in row]):
+    if not all((isinstance(element, int) or isinstance(element, float))
+               for element in [number for row in m_b for number in row]):
         raise TypeError("m_b should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
@@ -56,7 +56,7 @@ def matrix_mul(m_a, m_b):
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-        
+
     matrix1 = []
     for i in range(len(m_b[0])):
         my_row = []
@@ -74,4 +74,4 @@ def matrix_mul(m_a, m_b):
             my_row.append(product)
         matrix2.append(my_row)
 
-     return matrix2
+    return matrix2
