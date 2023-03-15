3279089e1889fe1250982d80bab5a0d4a4a182df
MJ-ONeill <mjoneill20100@gmail.com>
2023-03-15 00:18:14 +0000

    3 line print

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myVarC = 'C is fun';
+const myVarPython = 'Python is cool';
+const myVar = 'JavaScrip is amazing';
+
+console.log(myVarC);
+console.log(myVarPython);
+console.log(myVar);
