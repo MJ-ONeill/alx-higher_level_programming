c07ccac6892828c643928ab724615abfa423232e
MJ-ONeill <mjoneill20100@gmail.com>
2023-04-25 18:20:23 -0700

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', function (err, result) {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
 	if (err) {
-		console.log(err);
-	} else {
-		console.log(rtesult);
+		return console.log(err);
 	}
+	process.stdout.write(data);
 });
